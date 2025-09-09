317f5b4cb65c75a7f3e1af86ddb4d714dbaed713
Leandro Tenorio <145710165+leandrotnorio@users.noreply.github.com>
2025-09-09 00:42:26 +0000

    updtae de metodos

--- Program.cs
+++ Program.cs
@@ -5,5 +5,30 @@ Televisao tv = new Televisao(55f);
 
 Console.WriteLine($"A tv é do tamanho {tv.Tamanho}");
 
+tv.LigarTelevisao();
+
 tv.AumentarVolume();
 Console.WriteLine($"Volume {tv.Volume}");
+
+tv.AumentarVolume();
+Console.WriteLine($"Volume {tv.Volume}");
+
+tv.AbaixarVolume();
+Console.WriteLine($"Volume {tv.Volume}");
+
+
+tv.SubirCanal();
+Console.WriteLine($"Canal {tv.Canal}");
+
+tv.DescerCanal();
+Console.WriteLine($"Canal {tv.Canal}");
+
+tv.DesligarTelevisao();
+
+
+
+
+
+//tv.AbaixarVolume();
+//Console.WriteLine($"Volume {tv.AbaixarVolume}");
+
--- televisao.cs
+++ televisao.cs
@@ -1,15 +1,23 @@
 public class Televisao
 {
     private const int VOL_MAX = 100;
+    private const int VOL_MIN = 0;
+
+    private const int CAN_MAX = 520;
+    private const int CAN_MIN = 2;
+
+    private const bool TV_LIG = true;
+    private const bool TV_DESLIG = false;
+
     public Televisao(float tamanho)
     {
         Tamanho = tamanho;
     }
-    public float Tamanho { get; set;}
+    public float Tamanho { get; set; }
     public int Resolucao { get; set; }
     public int Volume { get; set; }
-    public int Canal { get; set;}
-    public bool Estado { get; set;}
+    public int Canal { get; set; }
+    public bool Estado { get; set; }
 
     public void AumentarVolume()
     {
@@ -18,4 +26,45 @@ public class Televisao
         else
             Console.WriteLine("A TV ja esta no maximo");
     }
+
+    public void AbaixarVolume()
+    {
+        if (Volume > VOL_MIN)
+            Volume--;
+        else
+            Console.WriteLine("A TV ja está no mínimo");
+    }
+
+    public void SubirCanal()
+    {
+        if (Canal <= CAN_MIN)
+            Canal++;
+        else
+            Console.WriteLine("canal maximo");
+    }
+    public void DescerCanal()
+    {
+        if (Canal >= CAN_MAX)
+            Canal--;
+        else
+            Console.WriteLine("canal minimo");
+    }
+
+    public void LigarTelevisao()
+    {
+        if (Estado == TV_LIG)
+            Estado = true;
+        else
+            Console.WriteLine("TV ligada");
+    }
+
+    public void DesligarTelevisao()
+    {
+        if (Estado == TV_DESLIG)
+            Estado = false;
+
+        Console.WriteLine("TV desligada");
+        
+    }
+
 }
